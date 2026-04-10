9dae82cfe7a6aa66971cf7737dd9844c887cc572
shimojok <shimojok@terraviss.com>
2026-04-11 03:07:30 +0900

    Create index.ts

---
+++ dashboard/src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as CapitalFlowVisualizer } from './CapitalFlowVisualizer';
+export { default as CarbonLedger } from './CarbonLedger';
+export { default as ImpactSimulator } from './ImpactSimulator';
+export { default as YieldPredictor } from './YieldPredictor';
+export { default as FinanceEngine } from './FinanceEngine';
