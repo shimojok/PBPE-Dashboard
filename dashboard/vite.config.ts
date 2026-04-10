6b9d4754c56fab6a52f9081cb23875706960b10e
shimojok <shimojok@terraviss.com>
2026-04-11 03:03:06 +0900

    Create vite.config.ts

---
+++ dashboard/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@models': path.resolve(__dirname, './src/models'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+});
